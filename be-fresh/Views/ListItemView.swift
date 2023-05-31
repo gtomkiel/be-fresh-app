47fabdf3239612e574f8b4949dda3fe34ebdd9db
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2023-05-31 10:31:14 +0200

    the list view for product list

--- be-fresh/Views/ListItemView.swift
+++ be-fresh/Views/ListItemView.swift
@@ -1,20 +1,63 @@
-//
 //  ListItemView.swift
-//  be-fresh
-//
-//  Created by Grzegorz Tomkiel on 31/05/2023.
+//  fresh
 //
 
 import SwiftUI
 
 struct ListItemView: View {
+    var name: String
+    var date: String
+    var showLine: Bool
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(alignment: .center) {
+                HStack(alignment: .center) {
+                    // point
+                    Circle()
+                        .fill(Color.white)
+                        .frame(width: 10.0, height: 10)
+                    
+                    // item
+                    VStack(alignment:.leading, spacing: 0) {
+                        Text(name)
+                            .font(.system(size: 20))
+                            .fontWeight(.bold)
+                        Text(date)
+                            .font(.system(size: 15))
+                    }
+                    .padding(.leading)
+                }
+                .foregroundColor(Color.white)
+                
+                Spacer()
+                Image("ItemBtn")
+            }
+            .padding(10)
+            
+            if (showLine) {
+                Image("Line")
+            }
+        }
     }
 }
 
+// preview
 struct ListItemView_Previews: PreviewProvider {
     static var previews: some View {
-        ListItemView()
+        ZStack(alignment: .leading) {
+            RoundedRectangle(cornerRadius: 10)
+                .fill(Color(red: 0.506, green: 0.718, blue: 0.345))
+                .shadow(radius: 5)
+            VStack(alignment: .leading) {
+                ListItemView(name: "NAME", date: "2023-01-03", showLine: true)
+                
+                ListItemView(name: "NAME", date: "2023-01-03", showLine: true)
+                
+                ListItemView(name: "NAME", date: "2023-01-03", showLine: true)
+                Spacer()
+            }
+            .padding([.top, .leading, .trailing])
+        }
     }
 }
