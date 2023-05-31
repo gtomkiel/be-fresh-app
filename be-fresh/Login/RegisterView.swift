48ce109dbd0358f08863f5fdc70d79c03384589d
Bohdan Zakusylo <bohdan.zakusylo@student.nhlstenden.com>
2023-05-31 10:39:59 +0200

    added the propper files

--- be-fresh/Login/RegisterView.swift
+++ be-fresh/Login/RegisterView.swift
@@ -1,20 +1,68 @@
-//
-//  RegisterView.swift
-//  be-fresh
-//
-//  Created by Grzegorz Tomkiel on 31/05/2023.
-//
-
 import SwiftUI
 
-struct RegisterView: View {
+struct RegistrationView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @State private var confirmPassword: String = ""
+    @State var UserName = UserDefaults.standard.string(forKey: "UserName")
+    @State var PassWord = UserDefaults.standard.string(forKey: "Password")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("Registration")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(.bottom, 20)
+            
+            TextField("Username", text: $username)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+                .padding(.bottom, 10)
+            
+            SecureField("Password", text: $password)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+                .padding(.bottom, 10)
+            
+            SecureField("Confirm Password", text: $confirmPassword)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.horizontal)
+                .padding(.bottom, 20)
+            
+            Button(action: register) {
+                Text("Register")
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            .padding(.horizontal)
+            
+            Spacer()
+        }
+        .padding()
+    }
+    
+    func register() {
+        // Perform registration logic here
+        // You can access the entered username and password using the `username` and `password` variables
+        // Validate the input, store user data, etc.
+        
+        // Example validation: check if passwords match
+        if password == confirmPassword {
+            // Registration successful
+            print("Registration successful!")
+        } else {
+            // Passwords do not match
+            print("Passwords do not match")
+        }
     }
 }
 
-struct RegisterView_Previews: PreviewProvider {
+struct RegistrationView_Previews: PreviewProvider {
     static var previews: some View {
-        RegisterView()
+        RegistrationView()
     }
 }
