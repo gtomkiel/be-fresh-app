538d7d8a4d6e7c0468b3df288b70a4c9897db56d
SaraRodrigues1 <sara.rodrigues.cardoso@student.nhlstenden.com>
2023-05-31 11:05:10 +0200

    Updated settings page

--- be-fresh/Views/SettingsView.swift
+++ be-fresh/Views/SettingsView.swift
@@ -1,15 +1,93 @@
-//
-//  SettingsView.swift
-//  be-fresh
-//
-//  Created by Grzegorz Tomkiel on 31/05/2023.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
+    @State private var showStatus = true
+    @State private var showStatus1 = true
+    @State private var showStatus2 = true
+    @State private var showStatus3 = true
+    @State private var showStatus4 = true
+    @State private var showStatus5 = true
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            HStack(alignment: .top) {
+                Text("Settings")
+                Spacer()
+            }
+            .font(.system(size: 45))
+            .fontWeight(.heavy)
+            .padding()
+            
+            VStack(spacing: 15) {
+                Rectangle()
+                    .frame(height: 40)
+                    .foregroundColor(Color(red: 217, green: 217, blue :217))
+                    .cornerRadius(15)
+                    .overlay {
+                        Toggle("Toggle option", isOn: $showStatus)
+                            .padding()
+                    }
+                    .padding([.leading, .trailing])
+                
+                Rectangle()
+                    .frame(height: 40)
+                    .foregroundColor(Color(red: 217, green: 217, blue: 217))
+                    .cornerRadius(15)
+                    .overlay {
+                        Toggle("Toggle option 1", isOn: $showStatus1)
+                            .padding()
+                    }
+                    .padding([.leading, .trailing])
+                
+                Rectangle()
+                    .frame(height: 40)
+                    .foregroundColor(Color(red: 217, green: 217, blue: 217))
+                    .cornerRadius(15)
+                    .overlay {
+                        Toggle("Toggle option 2", isOn: $showStatus2)
+                            .padding()
+                    }
+                    .padding([.leading, .trailing])
+                
+                Rectangle()
+                    .frame(height: 40)
+                    .foregroundColor(Color(red: 217, green: 217, blue: 217))
+                    .cornerRadius(15)
+                    .overlay {
+                        Toggle("Toggle option 3", isOn: $showStatus3)
+                            .padding()
+                    }
+                    .padding([.leading, .trailing])
+                
+                Rectangle()
+                    .frame(height: 40)
+                    .foregroundColor(Color(red: 217, green: 217, blue: 217))
+                    .cornerRadius(15)
+                    .overlay {
+                        Toggle("Toggle option 4", isOn: $showStatus4)
+                            .padding()
+                    }
+                    .padding([.leading, .trailing])
+                
+                Rectangle()
+                    .frame(height: 40)
+                    .foregroundColor(Color(red: 217, green: 217, blue: 217))
+                    .cornerRadius(15)
+                    .overlay {
+                        Toggle("Toggle option 5", isOn: $showStatus5)
+                            .padding()
+                    }
+                    .padding([.leading, .trailing])
+            }
+            
+            Rectangle()
+                .foregroundColor(Color(red: 217, green: 217, blue: 217))
+                .cornerRadius(15)
+                .padding()
+            
+            Spacer()
+            
+        }
     }
 }
 
