79d4342937f731bdf0ed648b524657616b803d72
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2023-06-02 11:33:44 +0200

    clear file and update code with ScrollView

--- be-fresh/Model/TabData.swift
+++ be-fresh/Model/TabData.swift
@@ -1,20 +1,45 @@
-//
-//  TabData.swift
-//  be-fresh
-//
-//  Created by Grzegorz Tomkiel on 31/05/2023.
-//
-
+import Foundation
 import SwiftUI
+import Combine
+import CoreData
 
-struct TabData: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+class DefautlModel: ObservableObject {
+
+    @Published var pageStatus: PageType = .main
+    @Published var isLoggedIn = false
+    @Published var showLogin = false
+    @Published var showRegister = false
+    
+    @Published var selectedTab : TabItem = .home
+    
+    @Published var first = UserDefaults.standard.bool(forKey: "FirstTime")
+}
+
+extension String {
+    func isValidatorEmail() -> Bool {
+        if self.count > 100 {
+            return false
+        }
+        let emailFormat = "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" + "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" + "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" + "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" + "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" + "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" + "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"
+        //let emailFormat = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        let emailPredicate = NSPredicate(format:"SELF MATCHES %@", emailFormat)
+        return emailPredicate.evaluate(with: self)
     }
 }
 
-struct TabData_Previews: PreviewProvider {
-    static var previews: some View {
-        TabData()
-    }
+enum LoginError: Error {
+    case UserNotExistsError(String), PasswordWrongError(String), UserExistsError(String), isNotValidateEmail(String)
+}
+
+enum PageType {
+    case login, register, main
+}
+
+enum TabItem : Hashable, Identifiable {
+    case products
+    case recipes
+    case home
+    case bookmarks
+    case settings
+    var id: TabItem { self }
 }
--- be-fresh/Views/HomePage.swift
+++ be-fresh/Views/HomePage.swift
@@ -14,68 +14,55 @@ struct HomePage: View {
         NavigationView(){
             GeometryReader { geometry in
                 ZStack {
-                    Color("backgroundColor")
-                    VStack {
-
-                        Button {
-                            UserDefaults.standard.set(false, forKey: "FirstTime")
-                            model.first = true
-                        } label: {
-                            Text("reset")
-                        }
-
-                        HStack {
-                            Text("Hello")
-                                .fontWeight(.bold)
-                                .font(.system(size: 48))
-                        }
-                        .padding(.horizontal, 32)
-                    
-
+                    ScrollView {
+                        Color("backgroundColor")
                         VStack {
-                            Text("Upcoming expire dates")
-                                .padding(.trailing, 74.0)
-                                .fontWeight(.semibold)
-                                .font(.system(size: 24))
-                            Rectangle()
-                                .foregroundColor(Color("greenColor"))
-                                .frame(width: 339, height: 264)
-                                .cornerRadius(15)
-                                .overlay {
-                                    Rectangle()
-                                        .foregroundColor(Color("lightGreen"))
-                                        .frame(width: 320, height: 50)
-                                        .cornerRadius(15)
-                                }
+                            HStack {
+                                Text("Hello user")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .fontWeight(.heavy)
+                                    .font(.system(size: 48))
+                            }
+                            .padding(.vertical, 20)
+                            
+                            
+                            VStack {
+                                Text("Upcoming expire dates")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .fontWeight(.semibold)
+                                    .font(.system(size: 24))
+                                Rectangle()
+                                    .foregroundColor(Color("greenColor"))
+                                    .frame(height: 264)
+                                    .cornerRadius(15)
+                                    .shadow(radius: 5)
+                            }
+                            .padding(.bottom, 20)
+                            
+                            VStack {
+                                Text("Todays recommendation")
+                                    .fontWeight(.semibold)
+                                    .font(.system(size: 24))
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Rectangle()
+                                    .foregroundColor(Color("greenColor"))
+                                    .frame(height: 204)
+                                    .cornerRadius(15)
+                                    .shadow(radius: 5)
+                                    .overlay{
+                                        Image("addButton")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(height: 59)
+                                            .position(x: 300, y: 250)
+                                    }
+                            }
+                            .padding(.bottom, 20)
+                            Spacer()
                         }
-                        
-                        VStack {
-                            Text("Todays recommendation")
-                                .fontWeight(.semibold)
-                                .font(.system(size: 24))
-                                .padding(.trailing, 70.0)
-                            Rectangle()
-                                .foregroundColor(Color("greenColor"))
-                                .frame(width: 339, height: 264)
-                                .cornerRadius(15)
-                                .overlay {
-                                    Rectangle()
-                                        .foregroundColor(Color("darkGreen"))
-                                        .frame(width: 320, height: 50)
-                                        .cornerRadius(15)
-                                }
-                                .overlay{
-                                    Image("addButton")
-                                        .resizable()
-                                        .scaledToFit()
-                                        .frame(width: 59, height: 59)
-                                        .position(x: 300, y: 250)
-                                }
-                        }
-                        
-                        Spacer()
                     }
                 }
+                .padding([.leading, .trailing])
             }
         }
     }
