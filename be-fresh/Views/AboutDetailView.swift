b09969271341e5900c9c7e7461e3e55ac0ab0dd9
ZehuiWangTeddy <111961563+ZehuiWangTeddy@users.noreply.github.com>
2023-06-12 15:26:39 +0200

    update setting page with about info

---
+++ be-fresh/Views/AboutDetailView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct AboutDetailView: View {
+    var body: some View {
+        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elitIn molestie lacus ac congue")
+            .padding()
+    }
+}
+
+struct AboutDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutDetailView()
+    }
+}
