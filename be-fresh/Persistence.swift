b7f0c16bffb6bdbecf4282ba5a8d6cae91920dbf
Grzegorz Tomkiel <grzegorz.tomkiel@student.nhlstenden.com>
2023-06-20 10:05:22 +0200

    calendar event removed when deleting product from the list

--- be-fresh/Persistence.swift
+++ be-fresh/Persistence.swift
@@ -1,4 +1,5 @@
 import CoreData
+import EventKit
 
 struct PersistenceController {
     static let shared = PersistenceController()
@@ -10,7 +11,6 @@ struct PersistenceController {
         let request: NSFetchRequest<Product> = Product.fetchRequest()
         let requestBookmark: NSFetchRequest<BookMark> = BookMark.fetchRequest()
         
-        
         return result
     }()
 
@@ -21,7 +21,7 @@ struct PersistenceController {
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
@@ -31,7 +31,31 @@ struct PersistenceController {
         container.viewContext.automaticallyMergesChangesFromParent = true
     }
     
+    func calendarRemove(product: Product) {
+        let eventStore = EKEventStore()
+        
+        eventStore.requestAccess(to: .event) { [weak eventStore] granted, _ in
+            guard granted else {
+                print("event: access denied")
+                return
+            }
+            
+            guard let eventStore = eventStore else { return }
+            
+            let match = eventStore.predicateForEvents(withStart: product.expirationDate!, end: product.expirationDate!, calendars: eventStore.calendars(for: .event))
+            
+            let event = eventStore.events(matching: match)
+            
+            do {
+                try eventStore.remove(event[0], span: .thisEvent)
+            } catch {
+                print("event: error")
+            }
+        }
+    }
+    
     func deleteProduct(_ product: Product) {
+        calendarRemove(product: product)
         container.viewContext.delete(product)
 
         do {
@@ -42,21 +66,21 @@ struct PersistenceController {
         }
     }
     
-    func savePrName(product: Product, name: String){
+    func savePrName(product: Product, name: String) {
         let context = PersistenceController.shared.container.viewContext
-            context.perform {
-                product.productName = name
+        context.perform {
+            product.productName = name
                 
-                do {
-                    try context.save()
-                    print("New product name saved successfully.")
-                } catch {
-                    print("Failed to save new product name: \(error.localizedDescription)")
-                }
+            do {
+                try context.save()
+                print("New product name saved successfully.")
+            } catch {
+                print("Failed to save new product name: \(error.localizedDescription)")
             }
+        }
     }
     
-    func deleteoldProducts(){
+    func deleteoldProducts() {
         let request: NSFetchRequest<Product> = Product.fetchRequest()
         do {
             let products = try container.viewContext.fetch(request)
@@ -74,7 +98,7 @@ struct PersistenceController {
         }
     }
     
-    func getAllProducts()-> String{
+    func getAllProducts() -> String {
         var allProductsString = ""
         let request: NSFetchRequest<Product> = Product.fetchRequest()
         do {
@@ -89,7 +113,7 @@ struct PersistenceController {
         return allProductsString
     }
     
-    func saveBookmark(bookmark: BookMark, text: String){
+    func saveBookmark(bookmark: BookMark, text: String) {
         let context = PersistenceController.shared.container.viewContext
         context.perform {
             bookmark.bookmark = text
@@ -116,4 +140,3 @@ struct PersistenceController {
         }
     }
 }
-
